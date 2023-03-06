f49d9571270405caee367ef57b97d27302c85997
Stefan Lendl <ste.lendl@gmail.com>
2023-03-06 09:51:29 +0700

    module: add :desktop/xfce4 for nixos
    Config:
    
      modules.desktop.xfce4.enable = true;
    
    Co-authored-by: thaenalpha <nopanun@pm.me>
    Signed-off-by: Nopanun Laochunhanun (Thaen) <nopanun@pm.me>

---
+++ nixos/modules/desktop/xfce4.nix
@@ -0,0 +1,45 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.xfce4;
+    configDir = config.dotfiles.configDir;
+in {
+  options.modules.desktop.xfce4 = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+
+    services = {
+      redshift.enable = true;
+      picom.enable = true;
+      xserver = {
+        enable = true;
+        desktopManager = {
+          xterm.enable = false;
+          xfce.enable = true;
+        };
+        displayManager = {
+          defaultSession = "xfce";
+          # lightdm.enable = true;
+          # lightdm.greeters.mini.enable = true;
+
+          # Enable automatic login for the user.
+          # autoLogin.enable = true;
+          # autoLogin.user = "${config.user.name}";
+        };
+      };
+    };
+
+    user.packages = with pkgs; [
+      (mkIf (config.hardware.pulseaudio.enable || config.services.pipewire.pulse.enable)
+        xfce.xfce4-volumed-pulse)
+    ];
+
+    # suspend-then-hibernate as configured in modules.hardware.hibernate will not work,
+    # because xfce4-power-manager takes over these actions.
+    # This can be manually disabld:
+    # https://docs.xfce.org/xfce/xfce4-power-manager/faq
+  };
+}
